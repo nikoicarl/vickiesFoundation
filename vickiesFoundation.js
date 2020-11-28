0d0bae5573fd70840e61fbbe7502fc9f1c725998
nikoicarl <cnikoi@logig.tech>
2020-11-28 22:38:23 +0000

    bootstrap added

--- vickiesFoundation.js
+++ vickiesFoundation.js
@@ -3,6 +3,7 @@ const start = express();
 const partials = require('express-partial');
 const md5 = require('md5');
 const socket = require('socket.io');
+const path = require('path');
 
 
 
@@ -38,6 +39,9 @@ start.set('view engine', 'ejs');
 
 //set static files folder
 start.use(express.static('./stuff'));
+start.use('/css', express.static('./node_modules/bootstrap/dist/css'));
+start.use('/js', express.static('./node_modules/bootstrap/dist/js'));
+start.use('/js', express.static('./node_modules/jquery/dist'));
 
 
 //Zip setup
@@ -51,7 +55,7 @@ start.use(express.static('./stuff'));
 
 
 //Run server on specified port
-const server = start.listen(3090, function() {
+const server = start.listen(9090, function() {
     console.log('Website is running on server with port 9090');
 });
 
